c6fa0950ab273b7fe58b4a91a9ab2ca622438b20
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-21 21:42:27 +0530

    feat: create app

--- day07/src/App.js
+++ day07/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
+import Childcom from './components/Childcom';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [msg, setMsg]=useState("Default");
+
+  const receiveMessage=(message)=>{
+    setMsg(message);
+  }
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello, I'm Parent</h2>
+      <Childcom setMessage={receiveMessage}/>
+      <p>Parent: My child said : {msg}</p>
     </div>
   );
 }
